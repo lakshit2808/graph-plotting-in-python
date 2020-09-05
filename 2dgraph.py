5d23a56fec1967e825b364deed10823eb56d1307
Lakshit <68190707+lakshit2808@users.noreply.github.com>
2020-09-05 19:37:41 +0530

    Create 2dgraph.py

---
+++ 2dgraph.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+
+x =[]
+y = []
+
+for i in range(1,11):
+    x.append(i)
+
+for i in range(1 , 11):
+    y.append(pow(2 , i))
+
+print(x)
+print(y)
+
+plt.title('Graph : Power of 2')
+plt.xlabel('x : 1 to 10')
+plt.ylabel('Power of 2 for corresponding x ')
+
+plt.plot(x,y)
+plt.show()
